63cb42a81ecd987ff9ab6479d704c2a2348a3fc0
Margulan Moldabekov <moldabekov@gmail.com>
2016-04-13 19:08:11 +0600

    Bob solution
    Stucked with regexp for UTF-8 input

---
+++ python/bob/bob.py
@@ -0,0 +1,27 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Skeleton file for the Python "Bob" exercise.
+#
+
+import re
+
+
+def hey(what):
+
+    what = what.lstrip().rstrip()
+    capital = re.findall(ur'[A-ZА-Я]+', what)
+    lower = re.findall(ur'[a-zа-я]+', what)
+
+    if len(what) == 0:
+        return 'Fine. Be that way!'
+
+    if (len(lower) == 0) and (len(capital) > 0):
+        return 'Whoa, chill out!'
+
+    if (len(what) != 0) and (what[len(what) - 1] == '?'):
+        return 'Sure.'
+
+    return 'Whatever.'
+
+# print (hey('ÜMLäÜTS'))
